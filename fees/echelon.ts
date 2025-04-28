ec490d8542bc691d3885de333186fb6bbeb62f3e
0xwml <wesley@thalalabs.xyz>
2025-04-27 18:28:57 -0600

    [echelon] add movement fees

--- fees/echelon.ts
+++ fees/echelon.ts
@@ -16,23 +16,38 @@ const feesEndpoint = (endTimestamp: number, timeframe: string) =>
 
 const movementFeesEndpoint = (endTimestamp: number, timeframe: string) =>
   endTimestamp
-    ? feesQueryURL + timeframe + `&endTimestamp=${endTimestamp}` + "&network=movement_mainnet"
-    : feesQueryURL + timeframe + "&network=movement_mainnet";
+      ? feesQueryURL + timeframe + `&endTimestamp=${endTimestamp}` + "&network=movement_mainnet"
+      : feesQueryURL + timeframe + "&network=movement_mainnet";
 
-const config: Record<string, (endTimestamp: number, timeframe: string) => string> = {
-  [CHAIN.APTOS]: feesEndpoint,
-  [CHAIN.MOVE]: movementFeesEndpoint,
-}
-    
-const fetch = async (timestamp: number, _:any, options: FetchOptions) => {
-  const dayFeesQuery = (await fetchURL(config[options.chain](timestamp, "1D")))?.data;
+const fetch = async (timestamp: number) => {
+  const dayFeesQuery = (await fetchURL(feesEndpoint(timestamp, "1D")))?.data;
   const dailyFees = dayFeesQuery.reduce(
     (partialSum: number, a: IVolumeall) => partialSum + a.value,
     0
   );
 
+  const dayFeesMovementQuery = (await fetchURL(movementFeesEndpoint(timestamp, "1D")))?.data;
+  const dailyMovementFees = dayFeesMovementQuery.reduce(
+      (partialSum: number, a: IVolumeall) => partialSum + a.value,
+      0
+  );
+
+  const totalFeesQuery = (await fetchURL(feesEndpoint(0, "ALL")))?.data;
+  const totalFees = totalFeesQuery.reduce(
+    (partialSum: number, a: IVolumeall) => partialSum + a.value,
+    0
+  );
+
+  const totalFeesMovementQuery = (await fetchURL(movementFeesEndpoint(0, "ALL")))?.data;
+  const totalMovementFees = totalFeesMovementQuery.reduce(
+      (partialSum: number, a: IVolumeall) => partialSum + a.value,
+      0
+  );
+
   return {
-    dailyFees: dailyFees,
+    totalFees: totalFees + totalMovementFees,
+    dailyFees: dailyFees + dailyMovementFees,
+    timestamp,
   };
 };
 
